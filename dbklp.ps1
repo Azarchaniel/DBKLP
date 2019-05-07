060ee40effc90396bf4de8260235ba3b1c9cc1e6
Azarchaniel <csonkalubos@gmail.com>
2019-05-07 23:12:34 +0200

    Update dbklp.ps1
    Fixed bugs; mostly in cases, when backup file was already created (that shouldn't occur in usage, because it will be used only once per day).

--- dbklp.ps1
+++ dbklp.ps1
@@ -1,17 +1,29 @@
-$PSDefaultParameterValues['*:Encoding'] = 'utf8'
 $drive = Get-Volume -FileSystemLabel Lubos_Csonka	#get the info about disk with the name Lubos_Csonka
-$file = "Knihy\Databaze knih a LP.xlsm"			#path to file (without drive letter)
-$letter = $drive.DriveLetter + ":"			#join drive letter and colon. Otherwise path wouldnt work
+$file = "Knihy\Databaze knih a LP.xlsm"				#path to file
+$letter = $drive.DriveLetter + ":"					#join drive letter and colon. Otherwise path wouldnt work
 $path = Join-Path -Path $letter -ChildPath $file	#Join paths of drive letter and path to file
 
-if (((get-date)-(ls $path).LastWriteTime).days -lt 1) {
-if (Test-Path "C:\Users\csonkal\Downloads\Databaze knih a LP.xlsm") {if (!(Test-Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha")) {New-Item -ItemType directory -Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha"} (Move-Item "C:\Users\csonkal\Downloads\Databaze knih a LP.xlsm" -destination "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm")}
-#If there is file in Google Drive (folder in PC)***
-	#If there is not a folder Backup, create new folder Backup
-#***move file into folder Backup and rename it with current date
-if (!(Get-Module "BurntToast")) {Install-Module BurntToast -Scope CurrentUser}	#if there is not module, install it
-(Copy-Item $path -Destination "C:\Users\csonkal\Downloads\"); (New-BurntToastNotification -Text "Database of books was backed up.")}	#if file is older than 1 day, copy it into folder and send a Win10 notification
-#notification has a problem with showing special characters. GitHub issue sent.
-Get-ChildItem –Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha" | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item  #if file in Backup is older than 30 days, delete it
+try {
+    if (((get-date)-(ls $path).LastWriteTime).days -lt 1) { #if file is older than 1 day
+        if (Test-Path "C:\Users\csonkal\Downloads\Databaze knih a LP.xlsm") #If there is file in Google Drive (folder in PC)***
+	        {if (!(Test-Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha")) #If there is not a folder Backup
+                {New-Item -ItemType directory -Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha"} #create new folder Backup
+                if (Test-Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha") #If there is a folder Backup
+                    {if (Test-Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm") #if there is already backup file
+                        {(Remove-Item "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm")};  #delete backup file
+                (Move-Item "C:\Users\csonkal\Downloads\Databaze knih a LP.xlsm" -destination "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm")} #If there is a folder Backup, move file into it and rename it with current date
+            }
 
-#change path from Downloads to Google Drive local
+    if (!(Get-Module "BurntToast")) {Install-Module BurntToast -Scope CurrentUser}	#if there is not module, install it
+    (Copy-Item $path -Destination "C:\Users\csonkal\Downloads\"); (New-BurntToastNotification -Text "Databáza knih bola zálohovaná.")}	#if file is older than 1 day, copy it into folder and send a Win10 notification
+    
+    if (Test-Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha") {Get-ChildItem -Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha" | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item}  #if file in Backup is older than 30 days, delete it
+} catch [Microsoft.PowerShell.Commands.MoveItemCommand]{
+    Remove-Item -Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm"
+    Remove-Item -Path "C:\Users\csonkal\Downloads\Databaze knih a LP.xlsm"
+    New-BurntToastNotification -Text "Chyba pri presune súboru."
+}
+
+
+#TODO: change path from Downloads to Google Drive local
+#TODO: better Error handling
