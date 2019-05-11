ed718b62585374a590511daf5cf21022aaf15921
Azarchaniel <csonkalubos@gmail.com>
2019-05-11 11:02:07 +0200

    Update dbklp.ps1
    Add console writings to see progress.

--- dbklp.ps1
+++ dbklp.ps1
@@ -7,15 +7,16 @@ try {
     if (((get-date)-(ls $path).LastWriteTime).days -lt 1) {                     #if file is older than 1 day
         if (Test-Path "C:\Users\csonkal\Downloads\Databaze knih a LP.xlsm")     #If there is file in Google Drive (folder in PC)
 	        {if (!(Test-Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha"))  #If there is not a folder Backup
-                {New-Item -ItemType directory -Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha"}    #create new folder Backup
+                {(New-Item -ItemType directory -Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha");(Write-Host 'Vytvorená zložka DatabazeKnihZaloha')}    #create new folder Backup
                     if (Test-Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm")        #if there is already backup file with current date
-                        {(Remove-Item "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm")};   #delete backup file
-                (Move-Item "C:\Users\csonkal\Downloads\Databaze knih a LP.xlsm" -destination "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm")} #If there is a folder Backup, move file into it and rename it with current date
+                        {(Remove-Item "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm");(Write-Host 'Vymazaný súbor DBKLP'$(get-date -f yyMMdd)'.xlsm')};   #delete backup file
+                (Move-Item "C:\Users\csonkal\Downloads\Databaze knih a LP.xlsm" -destination "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm");((Write-Host 'Vytvorený súbor DBKLP'$(get-date -f yyMMdd)'.xlsm'))} #If there is a folder Backup, move file into it and rename it with current date
     if (!(Get-Module "BurntToast")) {Install-Module BurntToast -Scope CurrentUser}	#if there is not module, install it
-    (Copy-Item $path -Destination "C:\Users\csonkal\Downloads\"); (New-BurntToastNotification -Text "Databáza knih bola zálohovaná.")}	#if file is older than 1 day, copy it into folder and send a Win10 notification
+    (Copy-Item $path -Destination "C:\Users\csonkal\Downloads\"); (New-BurntToastNotification -Text "Databáza knih bola zálohovaná."); (Write-Host 'Databáza knih bola zálohovaná.')}	#if file is older than 1 day, copy it into folder and send a Win10 notification
     
     if (Test-Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha") {Get-ChildItem -Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha" | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item}  #if file in Backup is older than 30 days, delete it
 } catch [Microsoft.PowerShell.Commands.MoveItemCommand]{
+    Write-Host 'Nastala chyba!'
     Remove-Item -Path "C:\Users\csonkal\Downloads\DatabazeKnihZaloha\DBKLP$(get-date -f yyMMdd).xlsm"
     Remove-Item -Path "C:\Users\csonkal\Downloads\Databaze knih a LP.xlsm"
     C:\Users\csonkal\Desktop\LCs_osobne\dbklp.ps1      #call function recursively
