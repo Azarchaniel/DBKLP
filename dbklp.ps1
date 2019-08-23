334820173e509f88467a6e6943d05334fd7637bb
Azarchaniel <csonkalubos@gmail.com>
2019-08-23 20:45:53 +0200

    Update dbklp.ps1

--- dbklp.ps1
+++ dbklp.ps1
@@ -3,13 +3,14 @@ $file = "Knihy\Databaze knih a LP.xlsm"				#path to file
 $letter = $drive.DriveLetter + ":"					#join drive letter and colon. Otherwise path wouldnt work
 $path = Join-Path -Path $letter -ChildPath $file	#Join paths of drive letter and path to file
 
-if ($env:computername -eq "DESKTOP-KGPMJ57") #if it's gf's PC, use these variables
+#following if statemant is not tested
+if ($env:computername -eq "DESKTOP-KGPMJ57" -or ($env:computername -eq "DESKTOP-8S0PLJ1" -and $env:username -eq "zanet")) #if it's gf's PC (or my computer but her acount), use these variables
 {
     $GdriveS = "C:\Users\zanet\Disk Google\Databáze knih\Databaze knih a LP.xlsm"
     $GdriveZ = "C:\Users\zanet\Disk Google\Databáze knih\DatabazeKnihZaloha"
     $GdriveD = "C:\Users\zanet\Disk Google\Databáze knih\"
 }
- elseif ($env:computername -eq "DESKTOP-8S0PLJ1") #if it's mine 
+ elseif ($env:computername -eq "DESKTOP-8S0PLJ1" -and ($env:username -eq "LubosCs")) #if it's mine pc
 {
     $GdriveS = "C:\Users\LubosCs\Disk Google\Databáze knih\Databaze knih a LP.xlsm"
     $GdriveZ = "C:\Users\LubosCs\Disk Google\Databáze knih\DatabazeKnihZaloha"
@@ -29,7 +30,7 @@ try {
     if (!(Get-Module -ListAvailable -Name BurntToast)) {(Install-Module BurntToast -Scope CurrentUser);(Write-Host 'Nainštalovaný modul BurntToast.')}	#if there is not module, install it
     (Copy-Item $path -Destination $GdriveD); (New-BurntToastNotification -Text "Databáza knih bola zálohovaná."); (Write-Host 'Databáza knih bola zálohovaná.')}	#if file is older than 1 day, copy it into folder and send a Win10 notification
     
-    if (Test-Path $GdriveZ) {Get-ChildItem -Path $GdriveZ | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item | Write-Host 'Vymazaný súbor starší než 30 dní.' | }  #if file in Backup is older than 30 days, delete it
+    if (Test-Path $GdriveZ) {Get-ChildItem -Path $GdriveZ | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item | Write-Host 'Vymazaný súbor starší než 30 dní.' }  #if file in Backup is older than 30 days, delete it
 } catch [Microsoft.PowerShell.Commands.MoveItemCommand]{
     Write-Host 'Nastala chyba!'
     Remove-Item -Path (Join-Path -Path $GdriveZ -ChildPath "DBKLP$(get-date -f yyMMdd).xlsm")
