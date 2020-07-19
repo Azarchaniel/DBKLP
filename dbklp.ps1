3d999bef4a2e75e5abdc2c305917e635535da470
Azarchaniel <csonkalubos@gmail.com>
2020-07-19 11:34:49 +0200

    Update dbklp.ps1
    Change of deleting strategy.

--- dbklp.ps1
+++ dbklp.ps1
@@ -10,7 +10,7 @@ if ($env:computername -eq "DESKTOP-KGPMJ57" -or ($env:computername -eq "DESKTOP-
     $GdriveZ = "C:\Users\zanet\Disk Google\Databáze knih\DatabazeKnihZaloha"
     $GdriveD = "C:\Users\zanet\Disk Google\Databáze knih\"
 }
- elseif ($env:computername -eq "DESKTOP-8S0PLJ1" -and ($env:username -eq "LubosCs")) #if it's mine pc
+ elseif ($env:computername -eq "DESKTOP-AFQ4MFR" -and ($env:username -eq "LubosCs")) #if it's mine pc
 {
     $GdriveS = "C:\Users\LubosCs\Disk Google\Databáze knih\Databaze knih a LP.xlsm"
     $GdriveZ = "C:\Users\LubosCs\Disk Google\Databáze knih\DatabazeKnihZaloha"
@@ -19,6 +19,12 @@ if ($env:computername -eq "DESKTOP-KGPMJ57" -or ($env:computername -eq "DESKTOP-
  else 
 {Write-Host "Unknown PC."; break}
 
+if ((Test-Path @($GdriveS, $GdriveZ, $GdriveD)) -contains $false) {
+    Write-Host "Nie je zložka Google Drive!"
+    Start-Sleep -s 1.5 #let console window open for 1.5s after finishing
+    break
+}
+
 try {
     if (((get-date)-(ls $path).LastWriteTime).days -lt 1) {                     #if file is older than 1 day
         if (Test-Path $GdriveS)     #If there is file in Google Drive (folder in PC)
@@ -26,16 +32,23 @@ try {
                 {(New-Item -ItemType directory -Path $GdriveZ);(Write-Host 'Vytvorená zložka DatabazeKnihZaloha')}    #create new folder Backup
                     if (Test-Path (Join-Path -Path $GdriveZ -ChildPath "DBKLP$(get-date -f yyMMdd).xlsm"))      #if there is already backup file with current date
                         {(Remove-Item (Join-Path -Path $GdriveZ -ChildPath "DBKLP$(get-date -f yyMMdd).xlsm"));(Write-Host 'Vymazaný súbor DBKLP'$(get-date -f yyMMdd)'.xlsm')};   #delete backup file
-                (Move-Item $GdriveS -destination (Join-Path -Path $GdriveZ -ChildPath "DBKLP$(get-date -f yyMMdd).xlsm"));((Write-Host 'Vytvorený súbor DBKLP'$(get-date -f yyMMdd)'.xlsm'))} #If there is a folder Backup, move file into it and rename it with current date
+                (Move-Item $GdriveS -destination (Join-Path -Path $GdriveZ -ChildPath "DBKLP$(get-date -f yyMMdd).xlsm"));((Write-Host 'Vytvorený súbor DBKLP'$(get-date -f yyMMdd)'.xlsm')) } #If there is a folder Backup, move file into it and rename it with current date
     if (!(Get-Module -ListAvailable -Name BurntToast)) {(Install-Module BurntToast -Scope CurrentUser);(Write-Host 'Nainštalovaný modul BurntToast.')}	#if there is not module, install it
-    (Copy-Item $path -Destination $GdriveD); (New-BurntToastNotification -Text "Databáza knih bola zálohovaná."); (Write-Host 'Databáza knih bola zálohovaná.')}	#if file is older than 1 day, copy it into folder and send a Win10 notification
-    
-    if (Test-Path $GdriveZ) {Get-ChildItem -Path $GdriveZ | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item | Write-Host 'Vymazaný súbor starší než 30 dní.' }  #if file in Backup is older than 30 days, delete it
+    (Copy-Item $path -Destination $GdriveD); (New-BurntToastNotification -Text "Databáza knih bola zálohovaná."); (Write-Host 'Databáza knih bola zálohovaná.')} #if file is older than 1 day, copy it into folder and send a Win10 notification
+
+    #if file in Backup is older than 30 days, delete it
+    #if (Test-Path $GdriveZ) {Get-ChildItem -Path $GdriveZ | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item | Write-Host 'Vymazaný súbor starší než 30 dní.' }
+
+    #if there's more than 10 files, delete the rest
+    if ((Get-ChildItem $GdriveZ | Measure-Object ).Count -igt 10) {
+        Write-Host "Viac nez desat suborov"
+        Get-ChildItem $GdriveZ | sort LastWriteTime | Select -first ((Get-ChildItem $GdriveZ | Measure-Object ).Count - 10) | Remove-Item
+    }
 } catch [Microsoft.PowerShell.Commands.MoveItemCommand]{
-    Write-Host 'Nastala chyba!'
+    Write-Host "Chyba pri presune súboru."
     Remove-Item -Path (Join-Path -Path $GdriveZ -ChildPath "DBKLP$(get-date -f yyMMdd).xlsm")
     Remove-Item -Path $GdriveS
-    C:\Users\csonkal\Desktop\LCs_osobne\dbklp.ps1      #call function recursively
+    #D:\Programy\dbklp.ps1      #call function recursively
     New-BurntToastNotification -Text "Chyba pri presune súboru."
 }
 
